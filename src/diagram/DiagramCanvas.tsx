09fd0e0caa6bcfe7b0e6b1fd4e0b13526bf7820c
Tadeáš Binder <bindetad@fel.cvut.cz>
2020-04-28 14:00:22 +0200

    28.4. fixes and improvements

--- src/diagram/DiagramCanvas.tsx
+++ src/diagram/DiagramCanvas.tsx
@@ -148,7 +148,7 @@ export default class DiagramCanvas extends React.Component<DiagramCanvasProps, D
                         if (ProjectElements[sid].connections.includes(id)) ProjectElements[sid].connections.splice(ProjectElements[sid].connections.indexOf(id),1);
                         if (vocabOrModal(ProjectLinks[id].iri)){
                             let domainOf = vocabOrModal(vocabOrModal(ProjectLinks[id].iri).domain).domainOf;
-                            if (domainOf){
+                            if (domainOf && ((ProjectLinks[id].iri).domain in VocabularyElements)){
                                 domainOf.splice(domainOf.indexOf(ProjectLinks[id].iri),1);
                             }
                         }
