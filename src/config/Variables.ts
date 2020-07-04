b2d96ab77d22f89b7e537b0c95b7a73bd0074204
Tadeáš Binder <bindetad@fel.cvut.cz>
2020-07-04 14:38:05 +0200

    ontoGrapher-63 rdfs:subClassOf relations are not loaded on the canvas

--- src/config/Variables.ts
+++ src/config/Variables.ts
@@ -99,7 +99,7 @@ export var VocabularyElements: {
             domain: string | undefined,
             range: string | undefined,
             types: string[],
-            domainOf: string[],
+            subClassOf: string[],
             restrictions: RestrictionObject[]
         }
 } = {};
