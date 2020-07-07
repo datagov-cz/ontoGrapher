e976bcda5b1c417a5194adc5fd1e1de5d9cf383a
Tadeáš Binder <bindetad@fel.cvut.cz>
2020-07-07 17:26:30 +0200

    ontoGrapher-65 Newly created concepts have wrong URI

--- src/panels/menu/MenuPanelAbout.tsx
+++ src/panels/menu/MenuPanelAbout.tsx
@@ -22,7 +22,7 @@ export default class MenuPanelAbout extends React.Component<Props, State> {
         return (<div className={"inert"}><Nav.Link onClick={() => {
             this.setState({modal: true});
         }}>
-            {"5 July - Changelog"}
+            {"7 July - Changelog"}
         </Nav.Link>
             <AboutModal modal={this.state.modal} close={() => {
                 this.setState({modal: false})
