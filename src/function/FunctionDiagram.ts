f9d7e52d5d5966e1a5ade589eb1ea7700e0a8998
Tadeáš Binder <bindetad@fel.cvut.cz>
2020-07-12 20:35:59 +0200

    ontoGrapher-70 Deleting a relationship in one diagram does not delete it in other diagrams

--- src/function/FunctionDiagram.ts
+++ src/function/FunctionDiagram.ts
@@ -72,12 +72,14 @@ export function loadDiagram(load: {
         cls.addTo(graph);
     }
     for (let link of load.links) {
-        let lnk = getNewLink(link.type, link.id);
-        lnk.source({id: link.source});
-        lnk.target({id: link.target});
-        lnk.labels(link.labels);
-        // @ts-ignore
-        lnk.vertices(link.vertices);
-        lnk.addTo(graph);
+        if (ProjectElements[link.source].connections.includes(link.id)) {
+            let lnk = getNewLink(link.type, link.id);
+            lnk.source({id: link.source});
+            lnk.target({id: link.target});
+            lnk.labels(link.labels);
+            // @ts-ignore
+            lnk.vertices(link.vertices);
+            lnk.addTo(graph);
+        }
     }
 }
