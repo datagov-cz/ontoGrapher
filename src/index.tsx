2f5b5a1ff4c15c10da15b3cbce58aa5bf3aefa7c
Tadeáš Binder <bindetad@fel.cvut.cz>
2020-08-04 20:58:50 +0200

    Preparation for merge

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import DiagramApp from './main/DiagramApp';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<DiagramApp
-	contextIRI={"https://slovník.gov.cz/datový/pracovní-prostor/pojem/metadatový-kontext/instance-1077205419"}
-	contextEndpoint={"http://localhost:7200/repositories/kodi-uloziste-dev"}
+	// contextIRI={"https://slovník.gov.cz/datový/pracovní-prostor/pojem/metadatový-kontext/instance-1077205419"}
+	// contextEndpoint={"http://localhost:7200/repositories/kodi-uloziste-dev"}
 />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
