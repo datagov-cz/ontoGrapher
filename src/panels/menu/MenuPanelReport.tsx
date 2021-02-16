4e2340483fba632000822482546be2b303962a83
Alice Binder <bindetad@fel.cvut.cz>
2021-02-16 14:31:06 +0100

    Replace Report URLs with environment values

--- src/panels/menu/MenuPanelReport.tsx
+++ src/panels/menu/MenuPanelReport.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Dropdown, SplitButton} from 'react-bootstrap';
-import {ProjectSettings} from "../../config/Variables";
+import {Environment, ProjectSettings} from "../../config/Variables";
 import {Locale} from "../../config/Locale";
 
 interface Props {
@@ -11,9 +11,6 @@ interface State {
 
 }
 
-const issueURL = "https://github.com/opendata-mvcr/sgov-assembly-line/issues/new?labels=bug&template=po-adavek-na-opravu.md&title=";
-const enhancementURL = "https://github.com/opendata-mvcr/sgov-assembly-line/issues/new?labels=enhancement&template=po-adavek-na-novou-funkcionalitu.md&title=";
-
 export default class MenuPanelReport extends React.Component<Props, State> {
 
 	render() {
@@ -23,10 +20,10 @@ export default class MenuPanelReport extends React.Component<Props, State> {
 				className={"inert report"}
 				title={Locale[ProjectSettings.viewLanguage].reportIssue}
 				variant={"warning"}
-				href={issueURL}
+				href={Environment.components.issueTracker.meta.newBug}
 				menuAlign={{sm: "left"}}
 				target={"_blank"}>
-				<Dropdown.Item href={enhancementURL} target={"_blank"}>
+				<Dropdown.Item href={Environment.components.issueTracker.meta.newFeature} target={"_blank"}>
 					{Locale[ProjectSettings.viewLanguage].reportEnhancement}
 				</Dropdown.Item>
 			</SplitButton>
