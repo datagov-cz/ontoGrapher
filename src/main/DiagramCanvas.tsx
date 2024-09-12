e0bedff2662f3d3e6a4c0df97f6f49367fd6b234
bindeali <56399637+bindeali@users.noreply.github.com>
2024-09-12 16:30:15 +0200

    Disable editing of a deleted connection (fixes associated crash)

--- src/main/DiagramCanvas.tsx
+++ src/main/DiagramCanvas.tsx
@@ -267,11 +267,10 @@ export default class DiagramCanvas extends React.Component<Props> {
           AppSettings.selectedLinks.includes(linkView.model.id as string) &&
           AppSettings.selectedLinks.length === 1
         )
-          addLinkTools(
-            linkView,
-            this.props.performTransaction,
-            this.props.updateElementPanel
-          );
+          addLinkTools(linkView, this.props.performTransaction, () => {
+            this.props.updateElementPanel();
+            this.props.updateDetailPanel(DetailPanelMode.HIDDEN);
+          });
       },
       /**
        * Mouse leave on cell:
